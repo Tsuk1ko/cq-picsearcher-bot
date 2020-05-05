e482f5728509c8fe33199d4fe8ef34fc8e09bef7
神代綺凜 <i@loli.best>
2020-05-05 15:22:09 +0800

    [feat] pm search result (#60) (#61)

--- main.js
+++ main.js
@@ -410,7 +410,7 @@ async function searchImg(context, customDB = -1) {
                 if (cache) {
                     hasCache = true;
                     for (const cmsg of cache) {
-                        replyMsg(context, `&#91;缓存&#93; ${cmsg}`);
+                        replySearchMsgs(context, `&#91;缓存&#93; ${cmsg}`);
                     }
                 }
             }
@@ -434,9 +434,7 @@ async function searchImg(context, customDB = -1) {
                     if ((setting.useAscii2dWhenLowAcc && saRet.lowAcc && (db == snDB.all || db == snDB.pixiv)) || (setting.useAscii2dWhenQuotaExcess && saRet.excess)) useAscii2d = true;
                     if (!saRet.lowAcc && saRet.msg.indexOf('anidb.net') !== -1) useWhatAnime = true;
                     if (saRet.msg.length > 0) needCacheMsgs.push(saRet.msg);
-
-                    replyMsg(context, saRet.msg);
-                    replyMsg(context, saRet.warnMsg);
+                    replySearchMsgs(context, saRet.msg, saRet.warnMsg);
                 }
 
                 //ascii2d
@@ -446,12 +444,11 @@ async function searchImg(context, customDB = -1) {
                     }));
                     if (asErr) {
                         const errMsg = (asErr.response && asErr.response.data.length < 50 && `\n${asErr.response.data}`) || '';
-                        replyMsg(context, `ascii2d 搜索失败${errMsg}`);
+                        replySearchMsgs(context, `ascii2d 搜索失败${errMsg}`);
                         console.error(`${getTime()} [error] ascii2d`);
                         console.error(asErr);
                     } else {
-                        replyMsg(context, color);
-                        replyMsg(context, bovw);
+                        replySearchMsgs(context, color, bovw);
                         needCacheMsgs.push(color);
                         needCacheMsgs.push(bovw);
                     }
@@ -560,7 +557,7 @@ function hasImage(msg) {
  * @param {boolean} at 是否at发送者
  */
 function replyMsg(context, msg, at = false) {
-    if (typeof msg !== 'string' || msg.length == 0) return;
+    if (typeof msg !== 'string' || msg.length === 0) return;
     switch (context.message_type) {
         case 'private':
             return bot('send_private_msg', {
@@ -578,6 +575,39 @@ function replyMsg(context, msg, at = false) {
                 message: at ? CQ.at(context.user_id) + msg : msg,
             });
     }
+}
+
+/**
+ * 回复搜图消息
+ *
+ * @param {object} context 消息对象
+ * @param {Array<string>} msgs 回复内容
+ */
+function replySearchMsgs(context, ...msgs) {
+    msgs = msgs.filter(msg => msg && typeof msg === 'string');
+    if (msgs.length === 0) return;
+    let promises = [];
+    // 是否私聊回复
+    if (setting.pmSearchResult) {
+        switch (context.message_type) {
+            case 'group':
+            case 'discuss':
+                if (!context.pmTipSended) {
+                    context.pmTipSended = true;
+                    replyMsg(context, '搜图结果将私聊发送！', true);
+                }
+                break;
+        }
+        promises = msgs.map(msg =>
+            bot('send_private_msg', {
+                user_id: context.user_id,
+                message: msg,
+            })
+        );
+    } else {
+        promises = msgs.map(msg => replyMsg(context, msg));
+    }
+    return Promise.all(promises);
 }
 
 /**
