daa4590450720b5108605502f936776780cf5e6e
神代綺凜 <i@loli.best>
2020-08-14 21:19:00 +0800

    feat: 群发消息手动限速 (resolve #86)

--- modules/broadcast.js
+++ modules/broadcast.js
@@ -1,29 +1,59 @@
+const sleep = ms => new Promise(resolve => setTimeout(resolve, ms));
+
+class AsyncQueue extends Array {
+  constructor(wait = 500) {
+    super();
+    this.running = false;
+    this.wait = wait;
+  }
+  push(...items) {
+    super.push(...items);
+    if (this.running == false) {
+      this.running = true;
+      setTimeout(async () => {
+        while (this.length) {
+          await this.shift()();
+          await sleep(this.wait);
+        }
+        this.running = false;
+      });
+    }
+  }
+}
+
+const queue = new AsyncQueue();
+
 export default async function broadcast(bot, args) {
   const sendTo = gid =>
-    bot('send_group_msg_rate_limited', {
+    bot('send_group_msg', {
       group_id: gid,
       message: args.broadcast,
     });
+
+  const queueSendTo = gid => queue.push(() => sendTo(gid));
+
   if (args.only) {
-    `${args.only}`.split(',').forEach(gid => sendTo(parseInt(gid)));
+    `${args.only}`.split(',').forEach(gid => queueSendTo(parseInt(gid)));
   } else {
     const groups = (await bot('get_group_list')).data.map(({ group_id }) => group_id);
     if (args.exclude) {
       const exclude = `${args.exclude}`.split(',').map(gid => parseInt(gid));
-      groups.filter(gid => !exclude.includes(gid)).forEach(gid => sendTo(gid));
+      groups.filter(gid => !exclude.includes(gid)).forEach(gid => queueSendTo(gid));
     } else if (args['only-admin']) {
       const selfqq = (await bot('get_login_info')).data.user_id;
-      groups.forEach(gid => {
-        bot('get_group_member_info_rate_limited', {
-          group_id: gid,
-          user_id: selfqq,
-          no_cache: true,
-        }).then(({ data: { role } }) => {
-          if (role !== 'member') sendTo(gid);
-        });
-      });
+      groups.forEach(gid =>
+        queue.push(() =>
+          bot('get_group_member_info', {
+            group_id: gid,
+            user_id: selfqq,
+            no_cache: true,
+          }).then(({ data: { role } }) => {
+            if (role !== 'member') return sendTo(gid);
+          })
+        )
+      );
     } else {
-      groups.forEach(gid => sendTo(gid));
+      groups.forEach(gid => queueSendTo(gid));
     }
   }
 }
