b8c998695dae7234fa37dd743c9069e2b2980534
神代綺凜 <i@loli.best>
2020-08-17 01:23:03 +0800

    fix: 定时提醒 interval 超出 32 位有符号整数导致刷屏 (fix #90)

--- modules/plugin/reminder.js
+++ modules/plugin/reminder.js
@@ -4,6 +4,7 @@ import Fse from 'fs-extra';
 import Parser from 'cron-parser';
 import minimist from 'minimist';
 import _ from 'lodash';
+import { setLargeTimeout, clearLargeTimeout } from '../utils/largeTimeout';
 
 const setting = config.bot.reminder;
 
@@ -51,7 +52,7 @@ function start(tid, interval, ctx, msg) {
   let next = -1;
   while (next < 0) next = interval.next().getTime() - now;
 
-  timeout[tid] = setTimeout(() => {
+  timeout[tid] = setLargeTimeout(() => {
     replyFunc(ctx, msg);
     start(tid, interval, ctx, msg);
   }, next);
@@ -59,7 +60,7 @@ function start(tid, interval, ctx, msg) {
 
 function stop(tid) {
   if (timeout[tid]) {
-    clearTimeout(timeout[tid]);
+    clearLargeTimeout(timeout[tid]);
     return true;
   } else return false;
 }
---
+++ modules/utils/largeTimeout.js
@@ -0,0 +1,21 @@
+const MAX_TIMEOUT = 2 ** 31 - 1;
+
+class LargeTimeout {
+  constructor(cb, ms) {
+    this.timeout = null;
+    const run = () => {
+      if (ms > MAX_TIMEOUT) {
+        ms -= MAX_TIMEOUT;
+        this.timeout = setTimeout(run, MAX_TIMEOUT);
+      } else {
+        this.timeout = setTimeout(cb, ms);
+      }
+    };
+  }
+  clear() {
+    clearTimeout(this.timeout);
+  }
+}
+
+export const setLargeTimeout = (cb, ms) => new LargeTimeout(cb, ms);
+export const clearLargeTimeout = lt => lt.clear();
