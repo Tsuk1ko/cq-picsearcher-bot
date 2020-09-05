7642aff012386e064975764ab41be8c13ef629d5
神代綺凜 <i@loli.best>
2020-09-06 02:32:15 +0800

    Create .eslintrc.js

---
+++ .eslintrc.js
@@ -0,0 +1,19 @@
+module.exports = {
+  env: {
+    es2021: true,
+    node: true,
+  },
+  extends: ['standard'],
+  parserOptions: {
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  rules: {
+    curly: 'off',
+    camelcase: 'off',
+    'no-case-declarations': 'off',
+    semi: ['error', 'always'],
+    'comma-dangle': ['error', 'only-multiline'],
+    'space-before-function-paren': 'off',
+  },
+};
