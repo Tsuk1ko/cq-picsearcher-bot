4f4b2feb467f0172255139b486016a5c887ca4cf
神代綺凜 <i@loli.best>
2020-12-01 18:14:36 +0800

    improve: 使用 node-cache 进行缓存

--- src/Logger.js
+++ src/Logger.js
@@ -1,6 +1,6 @@
 import Fs from 'fs';
 import Path from 'path';
-import logError from './logError';
+import NodeCache from 'node-cache';
 import { checkUpdate } from './utils/checkUpdate';
 
 const banListFile = Path.resolve(__dirname, '../data/ban.json');
@@ -28,21 +28,23 @@ function updateBanListFile() {
  */
 class Logger {
   constructor() {
-    this.searchMode = []; // 搜图模式
-    this.repeater = []; // 复读
-    this.searchCount = []; // 搜索次数记录
-    this.hsaSign = []; // 每日签到
+    this.searchMode = new NodeCache({ useClones: false, checkperiod: 1 }); // 搜图模式
+    this.repeater = new NodeCache({ useClones: false, stdTTL: 86400 }); // 复读
+    this.searchCount = new NodeCache({ useClones: false }); // 搜索次数记录
+    this.hsaSign = new Set(); // 每日签到
     this.date = new Date().getDate();
     this.dailyJobDone = false; // 每日任务是否完成
     this.checkUpdateAgo = Infinity;
+
+    this.searchMode.on('expired', (k, v) => v && v.cb && v.cb());
 
     setInterval(() => {
       // 每日初始化
       const nowDate = new Date().getDate();
       if (this.date !== nowDate) {
         this.date = nowDate;
-        this.searchCount = [];
-        this.hsaSign = [];
+        this.searchCount.flushAll();
+        this.hsaSign.clear();
         this.dailyJobDone = false;
         if (global.config.bot.checkUpdate >= 0) {
           if (this.checkUpdateAgo >= global.config.bot.checkUpdate) {
@@ -99,34 +101,26 @@ class Logger {
    * @memberof Logger
    */
   smSwitch(g, u, s, cb = null) {
-    if (!this.searchMode[g]) this.searchMode[g] = [];
-    if (!this.searchMode[g][u])
-      this.searchMode[g][u] = {
+    const key = `${g}-${u}`;
+    if (!this.searchMode.has(key)) {
+      this.searchMode.set(key, {
         enable: false,
         db: 999,
-        timeout: null,
-      };
-    const t = this.searchMode[g][u];
-    // 清除定时
-    if (t.timeout) {
-      clearTimeout(t.timeout);
-      t.timeout = null;
+      });
     }
+    const sm = this.searchMode.get(key);
+    sm.cb = cb;
     // 搜图模式切换
     if (s) {
       // 定时关闭搜图模式
-      if (g !== 0)
-        t.timeout = setTimeout(() => {
-          t.enable = false;
-          if (typeof cb === 'function') cb();
-        }, global.config.bot.searchModeTimeout * 1000);
-      if (t.enable) return false;
-      t.enable = true;
-      t.db = 999;
+      if (g !== 0) this.searchMode.ttl(key, Math.max(global.config.bot.searchModeTimeout, 0));
+      if (sm.enable) return false;
+      sm.enable = true;
+      sm.db = 999;
       return true;
     } else {
-      if (t.enable) {
-        t.enable = false;
+      if (sm.enable) {
+        sm.enable = false;
         return true;
       }
       return false;
@@ -142,7 +136,8 @@ class Logger {
    * @memberof Logger
    */
   smSetDB(g, u, db) {
-    this.searchMode[g][u].db = db;
+    const key = `${g}-${u}`;
+    if (this.searchMode.has(key)) this.searchMode.get(key).db = db;
   }
 
   /**
@@ -154,8 +149,9 @@ class Logger {
    * @memberof Logger
    */
   smStatus(g, u) {
-    if (!this.searchMode[g] || !this.searchMode[g][u] || !this.searchMode[g][u].enable) return false;
-    return this.searchMode[g][u].db;
+    const sm = this.searchMode.get(`${g}-${u}`);
+    if (!sm || !sm.enable) return false;
+    return sm.db;
   }
 
   /**
@@ -168,22 +164,20 @@ class Logger {
    * @memberof Logger
    */
   rptLog(g, u, msg) {
-    let t = this.repeater[g];
+    let rp = this.repeater.get(g);
     // 没有记录或另起复读则新建记录
-    if (!t || t.msg !== msg) {
-      this.repeater[g] = {
-        user: u,
+    if (!rp || rp.msg !== msg) {
+      rp = {
+        users: new Set([u]),
         msg: msg,
-        times: 1,
         done: false,
       };
-      t = this.repeater[g];
-    } else if (t.user !== u) {
+      this.repeater.set(g, rp);
+    } else if (!rp.users.has(u)) {
       // 不同人复读则次数加1
-      t.user = u;
-      t.times++;
+      rp.users.add(u);
     }
-    return t.done ? 0 : t.times;
+    return rp.done ? 0 : rp.users.size;
   }
 
   /**
@@ -193,7 +187,7 @@ class Logger {
    * @memberof Logger
    */
   rptDone(g) {
-    this.repeater[g].done = true;
+    if (this.repeater.has(g)) this.repeater.get(g).done = true;
   }
 
   /**
@@ -206,23 +200,31 @@ class Logger {
    * @memberof Logger
    */
   canSearch(u, limit, key = 'search') {
-    if (!this.searchCount[u]) this.searchCount[u] = {};
+    const sKey = `${u}-${key}`;
+    let sc = this.searchCount.get(sKey);
 
-    if (key === 'setu') {
-      if (!this.searchCount[u][key])
-        this.searchCount[u][key] = {
-          date: 0,
-          count: 0,
-        };
-      const setuLog = this.searchCount[u][key];
-      if (setuLog.date + limit.cd * 1000 <= new Date().getTime() && limit.value === 0) return true;
-      if (setuLog.date + limit.cd * 1000 > new Date().getTime() || setuLog.count >= limit.value) return false;
-      return true;
+    switch (key) {
+      case 'setu':
+        if (!sc) {
+          sc = {
+            date: 0,
+            count: 0,
+          };
+          this.searchCount.set(sKey, sc);
+        }
+        if (sc.date + limit.cd * 1000 <= new Date().getTime() && limit.value === 0) return true;
+        if (sc.date + limit.cd * 1000 > new Date().getTime() || sc.count >= limit.value) return false;
+        return true;
+
+      default:
+        if (limit === 0) return true;
+        if (!sc) {
+          sc = 0;
+          this.searchCount.set(sKey, sc);
+        }
+        if (sc < limit) return true;
     }
 
-    if (limit === 0) return true;
-    if (!this.searchCount[u][key]) this.searchCount[u][key] = 0;
-    if (this.searchCount[u][key] < limit) return true;
     return false;
   }
 
@@ -234,21 +236,26 @@ class Logger {
    * @memberof Logger
    */
   doneSearch(u, key = 'search') {
-    if (!this.searchCount[u]) this.searchCount[u] = {};
+    const sKey = `${u}-${key}`;
+    let sc = this.searchCount.get(sKey);
+
     switch (key) {
       case 'setu':
-        if (!this.searchCount[u][key])
-          this.searchCount[u][key] = {
+        if (!sc) {
+          sc = {
             date: 0,
             count: 0,
           };
-        const setuLog = this.searchCount[u][key];
-        setuLog.date = new Date().getTime();
-        setuLog.count++;
+          this.searchCount.set(sKey, sc);
+        }
+        sc.date = new Date().getTime();
+        sc.count++;
         break;
+
       default:
-        if (!this.searchCount[u][key]) this.searchCount[u][key] = 0;
-        this.searchCount[u][key]++;
+        if (!sc) sc = 0;
+        sc++;
+        this.searchCount.set(sKey, sc);
         break;
     }
   }
@@ -261,8 +268,8 @@ class Logger {
    * @memberof Logger
    */
   canSign(u) {
-    if (this.hsaSign.includes(u)) return false;
-    this.hsaSign.push(u);
+    if (this.hsaSign.has(u)) return false;
+    this.hsaSign.add(u);
     return true;
   }
 }
