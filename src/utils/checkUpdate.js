eb9673af32b8748d4bb770209dc81ab79ab6fbe8
神代綺凜 <i@loli.best>
2020-12-03 00:25:58 +0800

    fix: check update

--- src/utils/checkUpdate.js
+++ src/utils/checkUpdate.js
@@ -1,8 +1,9 @@
+import _ from 'lodash';
 import { execSync } from 'child_process';
 import { resolve } from 'path';
 import { compare } from 'compare-versions';
+import { get } from 'axios';
 import removeMd from 'remove-markdown';
-const get = require('axios');
 
 const { version } = require('../../package.json');
 const cwd = resolve(__dirname, '../../');
@@ -10,8 +11,10 @@ const cwd = resolve(__dirname, '../../');
 let lastCheck = '0.0.0';
 
 export const checkUpdate = async () => {
-  execSync('git fetch', { cwd });
-  const latestVersion = execSync('git describe --abbrev=0 --tags', { cwd }).toString().trim().replace(/^v/, '');
+  const latestTagRef = _.last(
+    _.compact(execSync('git ls-remote --tags --sort=committerdate', { cwd }).toString().split(/\s+/))
+  );
+  const latestVersion = _.head(_.last(latestTagRef.split('/v')).split('^'));
   if (lastCheck === latestVersion || compare(version, latestVersion, '>=')) return;
   const { data: fullChangelog } = await get(
     'https://cdn.jsdelivr.net/gh/Tsuk1ko/cq-picsearcher-bot@master/CHANGELOG.md'
