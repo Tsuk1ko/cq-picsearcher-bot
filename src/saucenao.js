6df0b75bacfa72270567efe6a3ed52033774aa7e
神代綺凜 <i@loli.best>
2021-03-08 01:10:41 +0800

    feat: 尽可能排除 pixiv 盗图结果

--- src/saucenao.js
+++ src/saucenao.js
@@ -1,8 +1,7 @@
+import _ from 'lodash';
 import nhentai from './nhentai';
 import getSource from './getSource';
 import CQ from './CQcode';
-// import shorten from './urlShorten/is.gd';
-// import { URL } from 'url';
 import pixivShorten from './urlShorten/pixiv';
 import logError from './logError';
 const Axios = require('./axiosProxy');
@@ -64,6 +63,7 @@ async function doSearch(imgURL, db, debug = false) {
               long_remaining, // 长时剩余
               similarity, // 相似度
               thumbnail, // 缩略图
+              index_id, // 图库
             },
             data: {
               ext_urls,
@@ -79,40 +79,32 @@ async function doSearch(imgURL, db, debug = false) {
           let source = null;
           if (ext_urls) {
             url = ext_urls[0];
-            // 如果结果有多个，优先取danbooru
-            if (ext_urls.length > 1) {
+            if (index_id === snDB.pixiv) {
+              // 如果结果为 pixiv，尝试找到原始投稿，避免返回盗图者的投稿
+              const pixivResults = data.results.filter(
+                result =>
+                  result.header.index_id === snDB.pixiv &&
+                  _.get(result, 'data.ext_urls[0]') &&
+                  Math.abs(result.header.similarity - similarity) < 5
+              );
+              if (pixivResults.length > 1) {
+                const resultData = _.minBy(pixivResults, result =>
+                  parseInt(result.data.ext_urls[0].match(/\d+/).toString())
+                ).data;
+                url = resultData.ext_urls[0];
+                title = resultData.title;
+                member_name = resultData.member_name;
+                member_id = resultData.member_id;
+              }
+            } else if (ext_urls.length > 1) {
+              // 如果结果有多个，优先取 danbooru
               for (let i = 1; i < ext_urls.length; i++) {
                 if (ext_urls[i].indexOf('danbooru') !== -1) url = ext_urls[i];
               }
-            } else {
-              // 如果结果为pixiv，尝试找到第一次投稿，从而避免返回盗图者的投稿
-              if (url.includes("pixiv")) {
-                let pixivResults = data.results.filter((result) =>
-                  (result.data.ext_urls && result.data.ext_urls[0].includes("pixiv"))
-                );
-                if (pixivResults.length > 1) {
-                  pixivResults.sort(function (a, b) {
-                    let pid1 = parseInt(
-                      a.data.ext_urls[0].match(/\d+/).toString()
-                    );
-                    let pid2 = parseInt(
-                      b.data.ext_urls[0].match(/\d+/).toString()
-                    );
-                    return pid1 - pid2;
-                  });
-                  let resultData = pixivResults[0].data;
-                  url = resultData.ext_urls[0];
-                  title = resultData.title;
-                  member_name = resultData.member_name;
-                  member_id = resultData.member_id;
-                }
-              }
             }
             url = url.replace('http://', 'https://');
-            // 若为danbooru则获取来源
-            if (url.includes("danbooru")) {
-              source = await getSource(url).catch(() => null);
-            }
+            // 获取来源
+            source = await getSource(url).catch(() => null);
           }
 
           if (!title) title = url.indexOf('anidb.net') === -1 ? ' 搜索结果' : ' AniDB';
