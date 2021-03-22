fbb8acd0a72590d5aecab249b2079338df2932ff
神代綺凜 <i@loli.best>
2021-03-22 18:01:03 +0800

    feat: 更新检查不再依赖本地 git

--- src/utils/checkUpdate.js
+++ src/utils/checkUpdate.js
@@ -1,31 +1,38 @@
 import _ from 'lodash';
-import { execSync } from 'child_process';
-import { resolve } from 'path';
-import { compare } from 'compare-versions';
 import { get } from 'axios';
 import removeMd from 'remove-markdown';
+import compareVersions from 'compare-versions';
+import { listServerRefs } from 'isomorphic-git';
+import http from 'isomorphic-git/http/node';
 
-const { version } = require('../../package.json');
-const cwd = resolve(__dirname, '../../');
+const { version, repository } = require('../../package.json');
+
+const repoUrl = repository.url.replace(/^git\+/, '');
+const repoName = repoUrl.split(/\/|\./).slice(-3, -1).join('/');
 
 let lastCheck = '0.0.0';
 
-const getLatestTagRef = () =>
-  _.last(_.compact(execSync('git ls-remote -qt --sort=v:refname', { cwd }).toString().split(/\s+/)));
+const getLatestVersion = async () => {
+  const refs = await listServerRefs({
+    http,
+    url: repoUrl.replace('github.com', 'github.com.cnpmjs.org'),
+    prefix: 'refs/tags/v2',
+  });
+  const versions = refs.map(({ ref }) => ref.replace('refs/tags/v', '')).sort(compareVersions);
+  return _.last(versions);
+};
 
 export const checkUpdate = async () => {
-  const latestVersion = getLatestTagRef().replace(/[^\d.]/g, '');
-  if (lastCheck === latestVersion || compare(version, latestVersion, '>=')) return;
-  const { data: fullChangelog } = await get(
-    `https://cdn.jsdelivr.net/gh/Tsuk1ko/cq-picsearcher-bot@v${latestVersion}/CHANGELOG.md`
-  );
+  const latestVersion = await getLatestVersion();
+  if (!latestVersion || lastCheck === latestVersion || compareVersions.compare(version, latestVersion, '>=')) return;
+  const { data: fullChangelog } = await get(`https://cdn.jsdelivr.net/gh/${repoName}@v${latestVersion}/CHANGELOG.md`);
   const changelogs = _.transform(
     fullChangelog.split(/\s*###\s*/),
     (arr, text) => {
       text = text.replace(/\n+## .+$/, '');
       const v = _.get(/^\d+-\d+[ \t]+[vV]([\d.]+)/.exec(text), 1);
       if (!v) return;
-      if (compare(version, v, '<')) {
+      if (compareVersions.compare(version, v, '<')) {
         arr.push(removeMd(text.trim(), { stripListLeaders: false }));
       }
     },
