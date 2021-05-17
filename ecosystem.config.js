b75042a407461b25b304c294e8d12692e9f3a699
神代綺凜 <i@loli.best>
2021-05-17 13:04:01 +0800

    fix: pm2 内存占用过高
    close #177

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,15 +1,15 @@
 module.exports = {
   apps: [
     {
-      name: 'CQPF',
+      name: 'cqps',
       script: 'index.js',
-      instances: 1,
       autorestart: true,
       max_restarts: 5,
       min_uptime: '10s',
       restart_delay: 5000,
-      out_file: './logs/normal.log',
-      error_file: './logs/error.log',
+      out_file: 'logs/normal.log',
+      error_file: 'logs/error.log',
+      combine_logs: true,
     },
   ],
 };
