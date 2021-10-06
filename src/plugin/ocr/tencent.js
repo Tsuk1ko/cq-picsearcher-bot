e97910c12cc221414fdd89d07526bc0d931d9563
神代綺凛 <i@loli.best>
2021-10-06 13:36:12 +0800

    chore: update dependencies

--- src/plugin/ocr/tencent.js
+++ src/plugin/ocr/tencent.js
@@ -4,9 +4,8 @@ import Fse from 'fs-extra';
 import event from '../../event';
 
 const {
-  common: { Credential, ClientProfile, HttpProfile },
   ocr: {
-    v20181119: { Client, Models },
+    v20181119: { Client },
   },
 } = require('tencentcloud-sdk-nodejs');
 
@@ -15,6 +14,7 @@ const LOG_PATH = Path.resolve(__dirname, '../../../data/tencent.ocr.json');
 const { SecretId, SecretKey, Region, useApi } = global.config.bot.ocr.tencent;
 
 let log = null;
+/** @type {Client} */
 let client = null;
 
 const init = () => {
@@ -27,12 +27,18 @@ const init = () => {
     if (old.m === log.m) Object.assign(log, old);
   }
   // client
-  const cred = new Credential(SecretId, SecretKey);
-  const httpProfile = new HttpProfile();
-  httpProfile.endpoint = 'ocr.tencentcloudapi.com';
-  const clientProfile = new ClientProfile();
-  clientProfile.httpProfile = httpProfile;
-  client = new Client(cred, Region || 'ap-beijing', clientProfile);
+  client = new Client({
+    credential: {
+      secretId: SecretId,
+      secretKey: SecretKey,
+    },
+    region: Region || 'ap-beijing',
+    profile: {
+      httpProfile: {
+        endpoint: 'ocr.tencentcloudapi.com',
+      },
+    },
+  });
 };
 
 init();
@@ -48,24 +54,13 @@ export default ({ url }) => {
   const usage = _.transform(useApi, (o, v) => o.push({ api: v, c: log[v] }), []);
   const min = _.minBy(usage, 'c');
 
-  const req = new Models.GeneralAccurateOCRRequest();
-  req.from_json_string(`{"ImageUrl":"${url}"}`);
+  if (min.c >= 950) {
+    throw new Error('API 免费额度可能即将用完，已自动阻止调用');
+  }
 
-  return new Promise((resolve, reject) => {
-    if (min.c >= 950) {
-      reject(new Error('API 免费额度可能即将用完，已自动阻止调用'));
-      return;
-    }
-    client[min.api](req, (errMsg, response) => {
-      if (errMsg) {
-        reject(errMsg);
-        return;
-      }
-
-      log[min.api]++;
-      Fse.writeJsonSync(LOG_PATH, log);
-
-      resolve(_.map(response.TextDetections, 'DetectedText'));
-    });
+  return client[min.api]({ ImageUrl: url }).then(res => {
+    log[min.api]++;
+    Fse.writeJsonSync(LOG_PATH, log);
+    return _.map(res.TextDetections, 'DetectedText');
   });
 };
