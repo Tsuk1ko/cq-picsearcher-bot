affc8e8a65b4cd9eda2a9f38b254160ff1ff1505
神代綺凛 <i@loli.best>
2022-03-05 16:36:54 +0800

    feat: 明日方舟公招计算器增加“资深干员”和“高级资深干员”无法被识别时的处理提示

--- src/plugin/akhr/akhr.draw.js
+++ src/plugin/akhr/akhr.draw.js
@@ -106,7 +106,10 @@ function getImg(AKDATA, results, recTags) {
 
   if (recTags.length < 5) {
     newLine();
-    drawCard('注意：词条识别出现遗漏，请将词条部分裁剪出来再试', false, colorPlan.white);
+    drawCard('注意：词条识别出现遗漏。如果含有“资深干员”或“高级资深干员”，', false, colorPlan.white);
+    x = axPadding;
+    y += fontSize + ratio * 4;
+    drawCard('请选中它们再截图；如果没有这俩词条请将词条部分裁剪出来再试。', false, colorPlan.white);
   }
 
   for (const { comb, chars } of results) {
