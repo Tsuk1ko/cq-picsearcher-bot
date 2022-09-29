155dbb19cee7617898d435634b343a99c38e5d2f
神代綺凛 <i@loli.best>
2022-09-30 00:23:33 +0800

    fix: axios 错误日志

--- src/logError.js
+++ src/logError.js
@@ -2,16 +2,18 @@ import _ from 'lodash';
 import { AxiosError } from 'axios';
 
 export default e => {
-  if (_.isObject(e) && e instanceof AxiosError) {
-    console.error(String(e));
+  if (e instanceof AxiosError) {
+    console.error(String(e.stack || e));
     console.error({
-      config: _.pick(e.config, ['method', 'url', 'data']),
-      response: {
-        ..._.pick(e.response, ['status', 'statusText']),
-        ...((e.response.headers['content-type'] || '').includes('application/json')
-          ? _.pick(e.response, ['data'])
-          : {}),
-      },
+      config: e.config ? _.pick(e.config, ['method', 'url', 'data']) : undefined,
+      response: e.response
+        ? {
+            ..._.pick(e.response, ['status', 'statusText']),
+            ...((e.response.headers['content-type'] || '').includes('application/json')
+              ? _.pick(e.response, ['data'])
+              : {}),
+          }
+        : undefined,
     });
     return;
   }
