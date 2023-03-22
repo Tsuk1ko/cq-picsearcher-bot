56594b67c16faa64ad32f5bbcb66d73d01476dd1
神代綺凜 <i@loli.best>
2023-03-22 14:47:40 +0800

    fix: Electron QQ 图片链接

--- src/index.mjs
+++ src/index.mjs
@@ -914,10 +914,13 @@ function debugMsgDeleteBase64Content(msg) {
 }
 
 function getUniversalImgURL(url = '') {
-  return url
-    .replace('/c2cpicdw.qpic.cn/offpic_new/', '/gchat.qpic.cn/gchatpic_new/')
-    .replace(/\/\d+\/+\d+-\d+-/, '/0/0-0-')
-    .replace(/\?.*$/, '');
+  if (/^https?:\/\/(c2cpicdw|gchat)\.qpic\.cn\/(offpic|gchatpic)_new\//.test(url)) {
+    return url
+      .replace('/c2cpicdw.qpic.cn/offpic_new/', '/gchat.qpic.cn/gchatpic_new/')
+      .replace(/\/\d+\/+\d+-\d+-/, '/0/0-0-')
+      .replace(/\?.*$/, '');
+  }
+  return url;
 }
 
 function isSendByAdmin(ctx) {
