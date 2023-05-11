f27aa533ab80d4090515ca603fb5f551101e6fdd
神代綺凜 <i@loli.best>
2023-05-11 16:18:35 +0800

    fix: 语言库随机逻辑

--- src/utils/math.mjs
+++ src/utils/math.mjs
@@ -5,7 +5,10 @@ import { sum } from 'lodash-es';
  */
 export const randomWithWeight = weights => {
   const weightSum = sum(weights);
-  const weightBoundaries = weights.map((weight, i) => (weights[i - 1] || 0) + weight / weightSum);
+  const weightBoundaries = [];
+  for (let i = 0; i < weights.length; i++) {
+    weightBoundaries[i] = (weightBoundaries[i - 1] || 0) + weights[i] / weightSum;
+  }
   const randomVal = Math.random();
   const randomIndex = weightBoundaries.findIndex(val => randomVal <= val);
   return randomIndex === -1 ? weights.length - 1 : randomIndex;
