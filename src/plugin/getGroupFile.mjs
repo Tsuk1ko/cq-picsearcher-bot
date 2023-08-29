346a7c0414c097f3cdf51d557c71d85fe6744e6b
神代綺凛 <i@loli.best>
2023-08-30 00:29:33 +0800

    fix: 已失效群文件获取报错

--- src/plugin/getGroupFile.mjs
+++ src/plugin/getGroupFile.mjs
@@ -12,53 +12,41 @@ export default async ctx => {
   const search = /^--get-group-file=(.+)/.exec(ctx.message);
   if (!search) return false;
 
-  const paths = String(search[1]).split('/');
+  const paths = String(CQ.unescape(search[1])).split('/');
   if (!paths.length || paths.length > 2) {
     global.replyMsg(ctx, '路径不正确', false, true);
     return true;
   }
 
-  const { data: root } = await global.bot('get_group_root_files', { group_id: ctx.group_id });
-  if (paths.length === 1) {
-    const file = root.files.find(({ file_name }) => file_name === paths[0]);
-    if (!file) {
-      global.replyMsg(ctx, `文件 ${CQ.escape(paths[0])} 不存在`, false, true);
-      return true;
-    }
+  let { data: dir } = await global.bot('get_group_root_files', { group_id: ctx.group_id });
+  let name = paths[0];
 
-    const {
-      data: { url },
-    } = await global.bot('get_group_file_url', {
-      group_id: ctx.group_id,
-      ..._.pick(file, ['file_id', 'busid']),
-    });
-    global.replyMsg(ctx, CQ.escape(handleFileUrl(url, file)), false, true);
-  } else {
-    const folder = root.folders.find(({ folder_name }) => folder_name === paths[0]);
+  if (paths.length > 1) {
+    const folder = dir.folders.find(({ folder_name }) => folder_name === name);
     if (!folder) {
-      global.replyMsg(ctx, `文件夹 ${CQ.escape(paths[0])} 不存在`, false, true);
+      global.replyMsg(ctx, `文件夹 ${CQ.escape(name)} 不存在`, false, true);
       return true;
     }
 
-    const { data: dir } = await global.bot('get_group_files_by_folder', {
+    const { data } = await global.bot('get_group_files_by_folder', {
       group_id: ctx.group_id,
       folder_id: folder.folder_id,
     });
-
-    const file = dir.files.find(({ file_name }) => file_name === paths[1]);
-    if (!file) {
-      global.replyMsg(ctx, `文件 ${CQ.escape(paths[1])} 不存在`, false, true);
-      return true;
-    }
-
-    const {
-      data: { url },
-    } = await global.bot('get_group_file_url', {
-      group_id: ctx.group_id,
-      ..._.pick(file, ['file_id', 'busid']),
-    });
-    global.replyMsg(ctx, CQ.escape(handleFileUrl(url, file)), false, true);
+    dir = data;
+    name = paths[1];
   }
+
+  const file = dir.files.find(({ file_name }) => file_name === name);
+  if (!file) {
+    global.replyMsg(ctx, `文件 ${CQ.escape(name)} 不存在`, false, true);
+    return true;
+  }
+
+  const { data } = await global.bot('get_group_file_url', {
+    group_id: ctx.group_id,
+    ..._.pick(file, ['file_id', 'busid']),
+  });
+  global.replyMsg(ctx, data ? CQ.escape(handleFileUrl(data.url, file)) : '该文件已失效', false, true);
 
   return true;
 };
