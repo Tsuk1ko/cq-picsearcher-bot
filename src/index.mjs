160492582ab8849a82488b38c4cd2f7e48ffc4b6
神代綺凛 <i@loli.best>
2024-03-16 12:28:57 +0800

    fix: LLOneBot 图片链接 #455

--- src/index.mjs
+++ src/index.mjs
@@ -974,6 +974,7 @@ function getUniversalImgURL(url = '') {
   if (/^https?:\/\/(c2cpicdw|gchat)\.qpic\.cn\/(offpic|gchatpic)_new\//.test(url)) {
     return url
       .replace('/c2cpicdw.qpic.cn/offpic_new/', '/gchat.qpic.cn/gchatpic_new/')
+      .replace('/gchat.qpic.cn/offpic_new/', '/gchat.qpic.cn/gchatpic_new/')
       .replace(/\/\d+\/+\d+-\d+-/, '/0/0-0-')
       .replace(/\?.*$/, '');
   }
